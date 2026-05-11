9add1dc01472232b4f07c27aaaf2e95ec4881c23
Maddineedi Rao <srinivasaraomaddineedi@gmail.com>
2026-05-11 16:07:14 +0530

    M2 addeded

--- M2.java
+++ M2.java
@@ -10,3 +10,21 @@ obj.stream().filter(x->x%2==0)
 
 
 }
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        int n = 10; // number of terms
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
