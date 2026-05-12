06ea10170dd13a88c5b87548049e7d5cf5012134
Maddineedi Srinivasrao <50824448+srinu6g@users.noreply.github.com>
2026-05-12 11:40:35 +0530

    Create M3.java

---
+++ M3.java
@@ -0,0 +1,13 @@
+class Sum{
+public static void main(String[] args)
+  {
+    int total=0;
+    int [] arr={1,2,3,4};
+    for(int i=0;i<arr.length;i++
+        {
+            total total+arr[i];
+    }
+    System.out.println(total);
+
+  }
+} 
